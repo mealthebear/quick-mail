ecea12e571e2bf3cdb14ecb80e9a20c88b238603
mealthebear <mealthebear@gmail.com>
2020-03-19 15:12:30 -0700

    Added bundle to gitignore and created scripts

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.js';
+
+const app = <App />;
+const root = document.getElementById('root');
+
+ReactDOM.render(app, root);
