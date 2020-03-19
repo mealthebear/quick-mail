8001376905e0fccc539fd0df10e3089baa6c1b16
mealthebear <mealthebear@gmail.com>
2020-03-19 16:23:15 -0700

    Implemented name input field and handlers

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        '& .MuliTextField-root': {
+            margin: theme.spacing(1),
+            width: 200,
+        },
+    },
+}));
 
 export default class Form extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            classes: '',
+            name: '',
+            email: '',
+            message: ''
+        };
+        this.checkState = this.checkState.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    checkState() {
+        console.log(this.state);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
 
     render() {
         return (
-            <div>This is the Form!</div>
+            <div>
+                <p onClick={this.checkState}>Let's check state!</p>
+                <form noValidate autoComplete='off'>
+                    <div>
+                        <TextField
+                            id='outlined-multiline-flexible'
+                            label='Name'
+                            multiline
+                            name='name'
+                            value={this.state.nameValue}
+                            onChange={this.handleChange}
+                            variant='outlined'
+                        />
+                    </div>
+                </form>
+            </div>
         )
     }
 }
