73a41a41267fd6571c0e633135bfe3c2d3265101
mealthebear <mealthebear@gmail.com>
2020-03-19 17:24:43 -0700

    Vertically stacked inputs and added spacing

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,15 +1,5 @@
 import React, { Component } from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-
-// const useStyles = makeStyles(theme => ({
-//     root: {
-//         '& .MuliTextField-root': {
-//             margin: theme.spacing(1),
-//             width: 200,
-//         },
-//     },
-// }));
 
 export default class Form extends Component {
     constructor(props) {
@@ -43,36 +33,43 @@ export default class Form extends Component {
                 <p onClick={this.checkState}>Let's check state!</p>
                 <form noValidate autoComplete='off'>
                     <div>
-                        <TextField
-                            error={ this.state.nameError ? true : false }
-                            helperText={ this.state.nameError ? 'Please enter a name' : null }
-                            id='outlined-multiline-flexible'
-                            label={ !this.state.nameError ? 'Name' : 'Error' }
-                            multiline
-                            name='name'
-                            onChange={this.handleChange}
-                            variant='outlined'
-                        />
-                        <TextField
-                            error={ this.state.emailError ? true : false }
-                            helperText={ this.state.emailError ? 'Please enter a valid e-mail' : null }
-                            id='outlined-multiline-flexible'
-                            label={ !this.state.emailError ? 'E-mail' : 'Error' }
-                            multiline
-                            name='email'
-                            onChange={this.handleChange}
-                            variant='outlined'
-                        />
-                        <TextField
-                            error={ this.state.messageError ? true : false }
-                            helperText={ this.state.messageError ? 'Please enter a message' : null }
-                            id='outlined-multiline-flexible'
-                            label={ !this.state.MessageError ? 'Message' : 'Error' }
-                            multiline
-                            name='message'
-                            onChange={this.handleChange}
-                            variant='outlined'
-                        />
+                        <div className="input-section">
+                            <TextField
+                                error={ this.state.nameError ? true : false }
+                                helperText={ this.state.nameError ? 'Please enter a name' : null }
+                                id='outlined-multiline-flexible'
+                                label={ !this.state.nameError ? 'Name' : 'Error' }
+                                mr={10}
+                                multiline
+                                name='name'
+                                onChange={this.handleChange}
+                                variant='outlined'
+                            />
+                        </div>
+                        <div className="input-section">
+                            <TextField
+                                error={ this.state.emailError ? true : false }
+                                helperText={ this.state.emailError ? 'Please enter a valid e-mail' : null }
+                                id='outlined-multiline-flexible'
+                                label={ !this.state.emailError ? 'E-mail' : 'Error' }
+                                multiline
+                                name='email'
+                                onChange={this.handleChange}
+                                variant='outlined'
+                            />
+                        </div>
+                        <div className="input-section">
+                            <TextField
+                                error={ this.state.messageError ? true : false }
+                                helperText={ this.state.messageError ? 'Please enter a message' : null }
+                                id='outlined-multiline-flexible'
+                                label={ !this.state.MessageError ? 'Message' : 'Error' }
+                                multiline
+                                name='message'
+                                onChange={this.handleChange}
+                                variant='outlined'
+                            />
+                        </div>
                     </div>
                 </form>
             </div>
